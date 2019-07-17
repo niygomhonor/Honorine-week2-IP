faa067560dea6c4d922a507570ce6a902acfd115
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-07-17 19:49:14 +0200

    adding switch statement

--- Honorine-js/index.js
+++ Honorine-js/index.js
@@ -1,56 +1,64 @@
 /*var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
 
 var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var century,weekDays,year,month,day;*/
-var formakan = document.getElementById('form1');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == formakan) {
-        formakan.style.display = "none";
-    }
-};
 
 var cc=parseInt(prompt("enter your century"));
 var yy=parseInt(prompt("enter last two numbers of year"));
 var mm=parseInt(prompt("enter born month"));
 var dd=parseInt(prompt("enter  date"));
 var gender=prompt("Enter gender");
-
+var weekDays;
 weekDays = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
 
  var day=console.log(weekDays.toFixed());
- 
-switch(day,gender){
-case(0,"male"):
+switch(gender){
+case "male":
+if(day===0){
 alert("Kwasi");
-break;
-
-case (1,"male"):
+}else if(day===1){
 alert("Kwadwo");
-break;
-case 2,"male":
-alert("Kwabena");
-break;
-case 3,"male":
-alert("Kwaku");
-break;
-case 4,"male":
-alert("Yaw");
-break;
-case 5,"male":
-alert("Kofi");
+}
+else if(day===2){
+    alert("Kwabena");
+}
+else if(day===3){
+    alert("Kwaku");
+}
+else if(day===4){
+    alert("Yaw");
+}
+else if(day===5){
+    alert("Kofi");
+}
+else if(day===6){
+    alert("Kwame");
+}
+var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+case "female":
+ if(day===0){
+    alert("Akosua");
+}
+    else if(day===1){
+        alert("Adwoa");
+    }
+        else if(day===2){
+            alert("Abenaa");
+        }
+            else if(day===3){
+                alert("Akua");
+            }
+                else if(day===4){
+                    alert("Yaa");}
 
-case 6,"male":
-alert("Kwame");
-break;
-  
-case 
-
-Default:
-alert("hhhh");
-
+                    else if(day===5){
+                        alert("Afua");
+                    }
+                    else if(_day===6){
+                        alert("Ama");
+                    }
+                    
 
 }
 
