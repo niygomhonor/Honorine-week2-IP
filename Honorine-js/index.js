4ba0d9d33f257fce223af339b3e40e23ff0ff2f1
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-07-18 08:33:38 +0200

    Using id condition

--- Honorine-js/index.js
+++ Honorine-js/index.js
@@ -7,61 +7,56 @@ var gender=prompt("Enter gender");
 var weekDays;
 weekDays = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
 
- var day=console.log(weekDays.toFixed());
+var day=console.log(weekDays.toFixed());
 
 
-while(gender==="male"){
 
-if(day===0){
+
+if(day===0 && gender==="male"){
 alert("Kwasi");
-}else if(day===1){
+}else if(day===1  && gender==="male"){
 alert("Kwadwo");
 }
-else if(day===2){
+else if(day===2 && gender==="male"){
     alert("Kwabena");
 }
-else if(day===3){
+else if(day===3 && gender==="male"){
     alert("Kwaku");
 }
-else if(day===4){
+else if(day===4 && gender==="male"){
     alert("Yaw");
 }
-else if(day===5){
+else if(day===5 && gender==="male"){
     alert("Kofi");
 }
-else if(day===6){
+else if(day===6 && gender==="male"){
     alert("Kwame");
 }
 
+else if(day===0&& gender==="female"){
+    alert("Akosua" );
 }
-
-while (gender==="female") {
-    
-
- if(day===0){
-    alert("Akosua");
-}
-else if(day===1){
+else if(day===1 && gender==="female"){
         alert("Adwoa");
 }
-else if(day===2){
+else if(day===2  && gender==="female"){
      alert("Abenaa");
 }
-else if(day===3){
+else if(day===3  && gender==="female"){
     alert("Akua");
 }
-else if(day===4){
+else if(day===4  && gender==="female"){
  alert("Yaa");
 }
 
-else if(day===5){
+else if(day===5  && gender==="female"){
     alert("Afua");
 }
-else if(_day===6){
+else if(_day===6  && gender==="female"){
     alert("Ama");
 }
          
 
-};
+
 
 
